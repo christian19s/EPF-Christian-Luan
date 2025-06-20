18cdd3b9620058daa7ffeb4d19c5c00f880fe3cd
Azeroqyu <zerosucksatlife@gmail.com>
2025-06-20 16:56:29 -0300

    Ajustando config para funcionar com docker

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 import os
 
+
 class Config:
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     # Configurações do servidor
-    HOST = 'localhost'
-    PORT = 8080
-    DEBUG = True
-    RELOADER = True
+    HOST = "0.0.0.0"
+    PORT = int(os.environ.get("PORT", 8080))
+    DEBUG = os.environ.get("DEBUG", "True") == "True"
+    RELOADER = os.environ.get("RELOADER", "True") == "True"
 
     # Paths
-    TEMPLATE_PATH = os.path.join(BASE_DIR, 'views')
-    STATIC_PATH = os.path.join(BASE_DIR, 'static')
-    DATA_PATH = os.path.join(BASE_DIR, 'data')
+    TEMPLATE_PATH = os.path.join(BASE_DIR, "views")
+    STATIC_PATH = os.path.join(BASE_DIR, "static")
+    DATA_PATH = os.path.join(BASE_DIR, "data")
 
     # Outras configurações
-    SECRET_KEY = 'sua-chave-secreta-aqui'
+    SECRET_KEY = "sua-chave-secreta-aqui"
