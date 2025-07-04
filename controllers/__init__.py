6a2cc7c58fe532ef5899c3c7a14a5539da225a67
Azeroqyu <zerosucksatlife@gmail.com>
2025-07-04 17:36:08 -0300

    arrumando conflito

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,8 +1,15 @@
 from bottle import Bottle
 
-from controllers.user_controller import user_routes
+# from controllers.activity_controller import activity_routes NAO COMMITA SEUS TESTES!!!
 from controllers.home_controller import home_routes
+from controllers.user_controller import user_routes
+from controllers.wiki_controller import wiki_routes
+
 
 def init_controllers(app: Bottle):
     app.merge(user_routes)
     app.merge(home_routes)
+    app.merge(wiki_routes)
+
+
+#    app.merge(activity_routes)
